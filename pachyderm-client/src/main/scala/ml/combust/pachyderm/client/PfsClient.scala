f4008aed6ca27114159773f68793275c18f90594
Hollin Wilkins <hollinrwilkins@gmail.com>
2017-02-05 23:08:55 -0800

    Implement more PFS client methods.

--- pachyderm-client/src/main/scala/ml/combust/pachyderm/client/PfsClient.scala
+++ pachyderm-client/src/main/scala/ml/combust/pachyderm/client/PfsClient.scala
@@ -1,11 +1,16 @@
 package ml.combust.pachyderm.client
 
+import java.io.OutputStream
+import java.nio.file.{FileSystems, Files}
+
+import com.google.protobuf.ByteString
 import pfs.pfs.APIGrpc.APIStub
 import pfs.pfs._
 import com.google.protobuf.empty.Empty
 import io.grpc.ManagedChannelBuilder
+import io.grpc.stub.StreamObserver
 
-import scala.concurrent.Future
+import scala.concurrent.{Future, Promise}
 
 /**
   * Created by hollinwilkins on 2/3/17.
@@ -48,6 +53,152 @@ class PfsClient(channel: _root_.io.grpc.Channel,
   def startCommit(parent: Option[Commit] = None,
                   provenance: Seq[Commit] = Seq()): Future[Commit] = {
     startCommit(StartCommitRequest(parent = parent, provenance = provenance))
+  }
+
+  def finishCommit(commit: Commit, cancel: Boolean = false): Future[Empty] = {
+    finishCommit(FinishCommitRequest(commit = Some(commit),
+      cancel = cancel))
+  }
+
+  def flushCommit(commits: Seq[Commit], toRepo: Seq[Repo] = Seq()): Future[CommitInfos] = {
+    flushCommit(FlushCommitRequest(commit = commits, toRepo = toRepo))
+  }
+
+  def archiveCommit(commits: Seq[Commit] = Seq()): Future[Empty] = {
+    archiveCommit(ArchiveCommitRequest(commits = commits))
+  }
+
+  def deleteCommit(commit: Commit): Future[Empty] = {
+    deleteCommit(DeleteCommitRequest(commit = Some(commit)))
+  }
+
+  def forkCommit(parent: Commit,
+                 branch: String,
+                 provenance: Seq[Commit] = Seq()): Future[Commit] = {
+    forkCommit(ForkCommitRequest(parent = Some(parent),
+      branch = branch,
+      provenance = provenance))
+  }
+
+  def inspectCommit(commit: Commit): Future[CommitInfo] = {
+    inspectCommit(InspectCommitRequest(commit = Some(commit)))
+  }
+
+  def listCommit(exclude: Seq[Commit] = Seq(),
+                 include: Seq[Commit] = Seq(),
+                 provenance: Seq[Commit] = Seq(),
+                 commitType: CommitType = CommitType.COMMIT_TYPE_NONE,
+                 status: CommitStatus = CommitStatus.ALL,
+                 block: Boolean = false): Future[CommitInfos] = {
+    listCommit(ListCommitRequest(exclude = exclude,
+      include = include,
+      provenance = provenance,
+      commitType = commitType,
+      status = status,
+      block = block))
+  }
+
+  def replayCommit(fromCommits: Seq[Commit],
+                   toBranch: String): Future[Commits] = {
+    replayCommit(ReplayCommitRequest(fromCommits = fromCommits, toBranch = toBranch))
+  }
+
+  def squashCommit(fromCommits: Seq[Commit],
+                   toCommit: Commit): Future[Empty] = {
+    squashCommit(SquashCommitRequest(fromCommits = fromCommits,
+      toCommit = Some(toCommit)))
+  }
+
+  def putLocalFile(commit: Commit,
+                   path: String,
+                   file: java.io.File,
+                   delimiter: Delimiter = Delimiter.NONE): Future[Empty] = {
+    putFile(File(Some(commit), path),
+      FileType.FILE_TYPE_REGULAR,
+      value = Some(Files.readAllBytes(FileSystems.getDefault.getPath(file.getPath))),
+      recursive = false)
+  }
+
+  def putFileUrl(commit: Commit,
+                 path: String,
+                 url: String,
+                 delimiter: Delimiter = Delimiter.NONE,
+                 recursive: Boolean = false): Future[Empty] = {
+    putFile(File(commit = Some(commit), path = path),
+      FileType.FILE_TYPE_REGULAR,
+      url = Some(url),
+      recursive = recursive)
+  }
+
+  def putFile(file: File,
+              fileType: FileType,
+              value: Option[Array[Byte]] = None,
+              url: Option[String] = None,
+              delimiter: Delimiter = Delimiter.NONE,
+              recursive: Boolean = false): Future[Empty] = {
+    val promise = Promise[Empty]()
+    val responseObserver = new StreamObserver[Empty] {
+      override def onError(t: Throwable): Unit = promise.failure(t)
+      override def onCompleted(): Unit = promise.success(Empty.defaultInstance)
+      override def onNext(value: Empty): Unit = { /* do nothing */ }
+    }
+
+    val bytes = value.map(ByteString.copyFrom).getOrElse(ByteString.EMPTY)
+    val observer = putFile(responseObserver)
+    observer.onNext(PutFileRequest(file = Some(file),
+      fileType = fileType,
+      value = bytes,
+      delimiter = delimiter,
+      url = url.getOrElse(""),
+      recursive = recursive))
+
+    promise.future
+  }
+
+  def getFullFile(commit: Commit,
+                  path: String,
+                  out: OutputStream): Future[Empty] = {
+    getFile(commit,
+      path,
+      out = out)
+  }
+
+  def getFile(commit: Commit,
+              path: String,
+              offsetBytes: Long = 0,
+              sizeBytes: Long = 0,
+              shard: Option[Shard] = None,
+              fromCommitId: Option[String] = None,
+              fullFile: Boolean = true,
+              out: OutputStream): Future[Empty] = {
+    val promise = Promise[Empty]()
+    val responseObserver = new StreamObserver[com.google.protobuf.wrappers.BytesValue] {
+      override def onError(t: Throwable): Unit = promise.failure(t)
+      override def onCompleted(): Unit = promise.success(Empty.defaultInstance)
+      override def onNext(value: com.google.protobuf.wrappers.BytesValue): Unit = {
+        out.write(value.toByteArray)
+      }
+    }
+
+    getFile(GetFileRequest(file = Some(File(Some(commit), path)),
+      offsetBytes = offsetBytes,
+      sizeBytes = sizeBytes,
+      shard = shard,
+      diffMethod = Some(DiffMethod(Some(Commit(commit.repo, fromCommitId.getOrElse(""))),
+        fullFile = fullFile))), responseObserver)
+
+    promise.future
+  }
+
+  def inspectFile(commit: Commit,
+                  path: String,
+                  shard: Option[Shard] = None,
+                  fromCommitId: Option[String] = None,
+                  fullFile: Boolean = true): Future[FileInfo] = {
+    inspectFile(InspectFileRequest(file = Some(File(Some(commit), path)),
+      shard = shard,
+      diffMethod = Some(DiffMethod(Some(Commit(commit.repo, fromCommitId.getOrElse(""))),
+        fullFile = fullFile))))
   }
 
   def deleteAll(): Future[Empty] = {
